f5f88ec165dc0bf6558536d386917abe1f0eaeb1
akbarshokh2008 <mosinovakbarjon9@gmail.com>
2024-03-15 14:15:23 +0500

    ыы

--- js/app.js
+++ js/app.js
@@ -1,5 +1,63 @@
-4-oy 8-dars
-4-oy 8-dars
-4-oy 8-dars
-4-oy 8-dars
-4-oy 8-dars
+// 1 - MASALA
+// let object = [];
+
+// function isEmpty(obj) {
+//   let result = Object.keys(obj).length === 0;
+//   return result;
+// }
+// console.log(isEmpty(object));
+
+// 2 - MASALA
+// let sarlavha = {
+//   john: 100,
+//   Ann: 100,
+//   Pete: 130,
+// };
+
+// function hisob(obj) {
+//   let result = Object.values(obj).reduce((acc, curr) => acc + curr, 0);
+//   return result;
+// }
+
+// console.log(hisob(sarlavha));
+
+// 3 - MASALA
+// let sarlavha = {
+//   john: 100,
+//   Ann: "salom",
+//   Pete: 130,
+// };
+// function ikkigaOshirilsin(obj) {
+//   for (let key in obj) {
+//     if (typeof obj[key] === "number") {
+//       obj[key] *= 2;
+//     }
+//   }
+//   return obj;
+// }
+// console.log(ikkigaOshirilsin(sarlavha));
+
+// 4 - MASALA
+// let arr = [5, 2, 1, -10, 8];
+
+// arr.sort((a, b) => b - a);
+
+// console.log(arr);
+
+// 5 - MASALA
+// function inputNumber() {
+//   const numbers = [];
+
+//   while (true) {
+//     const userInput = prompt("Iltimos, bir raqam kiriting:");
+
+//     if (userInput === null || isNaN(userInput)) {
+//       return numbers;
+//     } else {
+//       numbers.push(parseFloat(userInput));
+//     }
+//   }
+// }
+
+// const inputArray = inputNumber();
+// console.log("Kiritilgan raqamlar: ", inputArray);
